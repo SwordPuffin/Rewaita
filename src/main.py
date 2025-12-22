5325dd5c91c02a481db26d848cedd9882c894ae4
Nathan Perlman <145302069+SwordPuffin@users.noreply.github.com>
2025-12-21 19:59:44 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -149,7 +149,7 @@ X-Flatpak=io.github.swordpuffin.rewaita
         about = Adw.AboutDialog(application_name='Rewaita',
                                 application_icon='io.github.swordpuffin.rewaita',
                                 developer_name='Nathan Perlman',
-                                version='1.0.8',
+                                version='1.0.9',
                                 developers=['Nathan Perlman'],
                                 copyright='© 2025 Nathan Perlman')
         # Translators: Replace "translator-credits" with your name/username, and optionally an email or URL.
